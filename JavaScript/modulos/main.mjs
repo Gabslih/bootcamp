e09b80fb70cca0d38f8049f7205c677a1efbfe5c
Gabslih <lilian.gabby@gmail.com>
2022-07-26 20:02:26 -0300

    aula de módulos javascript

---
+++ JavaScript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Lilian", 26));
+console.log(mostraCidade('Lilian', 'Curitiba'));
+console.log(mostraHobby('Lilian', 'assistir séries'));
